2d0775a1b94a6669ade672e07924dbc5c0e8062f
Ohiz98 <ohiz9@yahoo.com>
2016-04-11 13:53:31 -0400

    Added files via upload

---
+++ Animal.java
@@ -0,0 +1,16 @@
+public class Animal{
+  
+  public enum Classe{MAMMAL, REPTILE};
+  
+  private Classe classe;
+  private String name;
+  private String kind; 
+  
+  public Animal(Classe classe, String name, String kind){
+    this.classe = classe;
+    this.name = name;
+    this.kind = kind;
+  
+    public void intro()
+      
+}
